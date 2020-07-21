4ca2bea591dd7c58dd3a143a227f7408c589a0b2
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-21 12:08:26 +0200

    Created PrintMessage method for output.

--- src/Uppgift2.Console/Program.cs
+++ src/Uppgift2.Console/Program.cs
@@ -7,12 +7,22 @@ namespace Uppgift2.Console
     {
 
         /// <summary>
-        /// Changes the foreground color in the terminal
+        /// Writes text on the console in given color.
         /// </summary>
-        /// <param name="color">The color to change to</param>
-        private static void ChangeForeGroundColor(ConsoleColor color)
+        /// <param name="text">A string with the text to write</param>
+        /// <param name="color">The color to write the text on</param>
+        /// <param name="newLine">If it should include a line terminator after the text</param>
+        private static void PrintMessage(string text, ConsoleColor color = ConsoleColor.Green, bool newLine = true)
         {
             System.Console.ForegroundColor = color;
+            if (newLine)
+            {
+                System.Console.WriteLine(text);
+            }
+            else
+            {
+                System.Console.Write(text);
+            }
         }
 
         /// <summary>
@@ -22,34 +32,30 @@ namespace Uppgift2.Console
         /// <returns>A string containing the user input</returns>
         private static string AskForString(string prompt)
         {
-            System.Console.Write(prompt);
+            PrintMessage(prompt, ConsoleColor.Yellow, newLine: false);
             return System.Console.ReadLine();
         }
 
         static void Main(string[] args)
         {
-            ChangeForeGroundColor(ConsoleColor.Green);
-
             var menuTitle = "********** Head Menu **********";
             var center = System.Console.WindowWidth / 2; //The center of the terminal window.
             var menuTitleAlignment = center + menuTitle.Length / 2; //Alignment for menu title
             var menuOptions = "0. Quit\n1: Buy movie tickets\n2. Iterate input ten times\n3. The third word";
 
-            System.Console.WriteLine($"{{0, {menuTitleAlignment}}}", menuTitle);
-            System.Console.WriteLine(menuOptions);
+            PrintMessage(string.Format($"{{0, {menuTitleAlignment}}}", menuTitle));
+            PrintMessage(menuOptions);
 
             var keepGoing = true;
-
             do
             {
-                ChangeForeGroundColor(ConsoleColor.Yellow);
                 var userOption = AskForString("Option: ");
-                ChangeForeGroundColor(ConsoleColor.Green);
 
                 switch (userOption)
                 {
                     case "0":
-                        System.Console.WriteLine("You chose to quit the application");
+                        // System.Console.WriteLine("You chose to quit the application");
+                        PrintMessage("Bye");
                         keepGoing = false;
                         break;
                     case "1":
@@ -62,9 +68,7 @@ namespace Uppgift2.Console
                         TheThirdWord();
                         break;
                     default:
-                        ChangeForeGroundColor(ConsoleColor.Red);
-                        System.Console.WriteLine("Wrong input: the value {0} is not a valid option", userOption);
-                        ChangeForeGroundColor(ConsoleColor.Green);
+                        PrintMessage(string.Format("Wrong input: the value {0} is not a valid option", userOption), ConsoleColor.Red);
                         break;
                 }
             } while (keepGoing);
